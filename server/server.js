a2f3fdc157fe903e43ff13357d41098b2e0ddac5
Nemanja-Milojevic <nemanja.milojevic@dev.telnet.rs>
2018-11-21 14:06:44 +0100

    'server-fix-2'

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api', api)
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(expres.static(path.join(__dirname, '/dist')));
 
 routes.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
