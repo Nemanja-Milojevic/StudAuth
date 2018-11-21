f82131d75d19a80d97d1add75968ca415d67588b
Nemanja-Milojevic <nemanja.milojevic@dev.telnet.rs>
2018-11-21 14:03:19 +0100

    'fix-server-1'

--- server/server.js
+++ server/server.js
@@ -11,10 +11,11 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api', api)
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('/', function(req, res){
-    res.send("Hello from server")
-})
+routes.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
